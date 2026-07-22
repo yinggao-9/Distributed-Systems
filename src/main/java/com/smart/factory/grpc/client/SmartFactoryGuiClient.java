be92b09e2f484106acff96e36e69030f0c6e15ee
yinggao-9 <tangyan986ku@gmail.com>
2026-07-22 11:24:24 +0100

    CA 0722-client

--- src/main/java/com/smart/factory/grpc/client/SmartFactoryGuiClient.java
+++ src/main/java/com/smart/factory/grpc/client/SmartFactoryGuiClient.java
@@ -3,24 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.smart.factory.grpc.client;
-import com.smartfactory.discovery.ServiceDiscoveryClient;
-import com.smartfactory.energy.grpc.EnergyOptimizationServiceGrpc;
-import com.smartfactory.energy.grpc.PowerUsageReport;
-import com.smartfactory.energy.grpc.SchedulingAdvice;
-import com.smartfactory.maintenance.grpc.*;
-import com.smartfactory.monitoring.grpc.DeviceMonitoringServiceGrpc;
-import com.smartfactory.monitoring.grpc.SensorReading;
-import com.smartfactory.monitoring.grpc.StreamAck;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.stub.StreamObserver;
-
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  *
